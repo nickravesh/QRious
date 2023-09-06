bc176752b7cdcb9a79073452e960b73de84e0b9e
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    improved the saving part of the script, now it saves finds the path of the script and store the output in a directory called history

--- makeQr.py
+++ makeQr.py
@@ -1,9 +1,10 @@
-import segno
-from subprocess import Popen # to run shell commands
-import os
+import segno # to make qr-codes
+import os # to run system commands
 
 userInput = input("Please Enter Your Text, The QR-code Will Be Generated:\n")
 qrcode = segno.make(userInput)
-qrcode.save("qrCode.png", scale=20, dark="yellow", light="#323524")
+
+# gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
+qrcode.save(f"{os.path.dirname(__file__)}/history/qrCode.png", scale=20, dark="yellow", light="#323524")
 
 os.system(f'/home/zed/.local/bin/segno --compact "{userInput}"')
