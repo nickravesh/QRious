3e2edc045704eec017632cb9ff9c9afe4944fd33
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    Add QR-code generation and display features.
    Import segno module to create QR-codes from user input.
    Use getpass module to get the username of the user running the script.
    Use os module to run system commands and modify environment variables.
    Save a colorful copy of the QR-code in the history directory.
    Show the QR-code in the terminal using segno command.

--- makeQr.py
+++ makeQr.py
@@ -1,5 +1,7 @@
 import segno # to make qr-codes
+from getpass import getuser # to get the username of the user running the script
 import os # to run system commands
+import sys
 
 userInput = input("Please Enter Your Text, The QR-code Will Be Generated:\n")
 qrcode = segno.make(userInput)
@@ -7,4 +9,7 @@ qrcode = segno.make(userInput)
 # gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
 qrcode.save(f"{os.path.dirname(__file__)}/history/qrCode.png", scale=20, dark="yellow", light="#323524")
 
-os.system(f'/home/zed/.local/bin/segno --compact "{userInput}"')
+# Add /home/zed/.local/bin to the PATH variable of the current process
+os.environ["PATH"] += os.pathsep + "/home/zed/.local/bin"
+
+os.system(f'segno --compact "{userInput}"') # to show the qr-code in the terminal
