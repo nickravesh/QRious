c3189ebed67a811833735822572be8a709bb8fb7
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    added history.log file to store value and name of the generated qr-codes

--- makeQr.py
+++ makeQr.py
@@ -39,14 +39,20 @@ def generate_QRcode():
         # This line of code uses the `min()` function with a lambda function as the key to find the oldest file in the `history` directory based on its creation time. The lambda function takes a filename as an argument, joins it with the directory path using `os.path.join()`, and then returns the creation time of the resulting file path using `os.path.getctime()`. The resulting filename is then passed to `os.remove()` to delete the file.
         os.remove(f"{os.path.dirname(__file__)}/history/{min(listOfFiles, key=lambda x: os.path.getctime(os.path.join(f'{os.path.dirname(__file__)}/history/', x)))}")
 
-
     # gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
-    qrcode.save(f"{os.path.dirname(__file__)}/history/{datetime.datetime.now().strftime('%Y_%m_%d')}-{datetime.datetime.now().strftime('%H%M%S')}.png", scale=20, dark="black", light="white")
+    fileName = ""
+    filePathToSave = f"{os.path.dirname(__file__)}/history/{datetime.datetime.now().strftime('%Y_%m_%d')}-{datetime.datetime.now().strftime('%H%M%S')}.png"
+    qrcode.save(filePathToSave, scale=20, dark="black", light="white")
 
     # Add /home/[username]/.local/bin to the PATH variable of the current process
     os.environ["PATH"] += os.pathsep + f"/home/{getuser()}/.local/bin"
 
     os.system(f'segno --compact "{userInput}"') # to show the qr-code in the terminal
+
+    qrHistory = {}
+
+    with open("./history/history.log", "a") as fileHandel:
+        fileHandel.write(f"{userInput}||{filePathToSave.split('/')[-1]}\n")
 
 
 def scan_QRcode():
