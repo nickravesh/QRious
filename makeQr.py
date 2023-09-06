3ef51cf6d706ab4a9906ecd3697db32b6e6b6aa0
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    created two functions, one for generating the qr-code and the second for controlling the user choice at the start of the program

--- makeQr.py
+++ makeQr.py
@@ -5,20 +5,30 @@ from getpass import getuser # to get the username of the user running the script
 import os # to run system commands
 import datetime
 
-userInput = input("Please Enter Your Text, The QR-code Will Be Generated:\n")
-qrcode = segno.make_qr(userInput) # make the qr-code using segno
 
-if len(os.listdir(f"{os.path.dirname(__file__)}/history")) == 10: # get the number of files that exists in the history directory
-    # get the list of the files in the history directory
-    listOfFiles = os.listdir(f"{os.path.dirname(__file__)}/history")
-    # This line of code uses the `min()` function with a lambda function as the key to find the oldest file in the `history` directory based on its creation time. The lambda function takes a filename as an argument, joins it with the directory path using `os.path.join()`, and then returns the creation time of the resulting file path using `os.path.getctime()`. The resulting filename is then passed to `os.remove()` to delete the file.
-    os.remove(f"{os.path.dirname(__file__)}/history/{min(listOfFiles, key=lambda x: os.path.getctime(os.path.join(f'{os.path.dirname(__file__)}/history/', x)))}")
+def generate_QRcode():
+    userInput = input("\nPlease Enter Your Text, The QR-code Will Be Generated:\n")
+    qrcode = segno.make_qr(userInput) # make the qr-code using segno
+
+    #removes the oldest qr-code in the history directory if there is more than 10 of them
+    if len(os.listdir(f"{os.path.dirname(__file__)}/history")) >= 10: # get the number of files that exists in the history directory
+        # get the list of the files in the history directory
+        listOfFiles = os.listdir(f"{os.path.dirname(__file__)}/history")
+        # This line of code uses the `min()` function with a lambda function as the key to find the oldest file in the `history` directory based on its creation time. The lambda function takes a filename as an argument, joins it with the directory path using `os.path.join()`, and then returns the creation time of the resulting file path using `os.path.getctime()`. The resulting filename is then passed to `os.remove()` to delete the file.
+        os.remove(f"{os.path.dirname(__file__)}/history/{min(listOfFiles, key=lambda x: os.path.getctime(os.path.join(f'{os.path.dirname(__file__)}/history/', x)))}")
 
 
-# gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
-qrcode.save(f"{os.path.dirname(__file__)}/history/{datetime.datetime.now().strftime('%Y_%m_%d')}-{datetime.datetime.now().strftime('%H%M%S')}.png", scale=20, dark="yellow", light="#35155D")
+    # gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
+    qrcode.save(f"{os.path.dirname(__file__)}/history/{datetime.datetime.now().strftime('%Y_%m_%d')}-{datetime.datetime.now().strftime('%H%M%S')}.png", scale=20, dark="yellow", light="#35155D")
 
-# Add /home/zed/.local/bin to the PATH variable of the current process
-os.environ["PATH"] += os.pathsep + "/home/zed/.local/bin"
+    # Add /home/[username]/.local/bin to the PATH variable of the current process
+    os.environ["PATH"] += os.pathsep + f"/home/{getuser()}/.local/bin"
 
-os.system(f'segno --compact "{userInput}"') # to show the qr-code in the terminal
+    os.system(f'segno --compact "{userInput}"') # to show the qr-code in the terminal
+
+
+
+status = input("What do you want to do?\n(G)enerate QR-Code, See the (H)istory? ")
+
+
+generate_QRcode()
