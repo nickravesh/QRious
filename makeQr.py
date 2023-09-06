11b8555c047c057ebc19660ec0e28789e0f2a782
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    now using segno instead of qrcode, in this version it will show the output directly in the terminal and also saves a colorful copy of it in the directory which the script runned

--- makeQr.py
+++ makeQr.py
@@ -1,7 +1,9 @@
 import segno
-
+from subprocess import Popen # to run shell commands
+import os
 
 userInput = input("Please Enter Your Text, The QR-code Will Be Generated:\n")
 qrcode = segno.make(userInput)
-qrcode.save("qrCode.png")
-qrcode.show(scale=20, dark="yellow", light="#323524")
+qrcode.save("qrCode.png", scale=20, dark="yellow", light="#323524")
+
+os.system(f'/home/zed/.local/bin/segno --compact "{userInput}"')
