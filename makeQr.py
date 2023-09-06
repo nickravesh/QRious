2f735cf80f65e346274601d1b3866de4aef26c24
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-06 10:23:30 +0330

    fix the saved qr-code not being scanned.

--- makeQr.py
+++ makeQr.py
@@ -4,10 +4,10 @@ import os # to run system commands
 import sys
 
 userInput = input("Please Enter Your Text, The QR-code Will Be Generated:\n")
-qrcode = segno.make(userInput)
+qrcode = segno.make_qr(userInput)
 
 # gets the path of the script directory and generates a colorful copy of qr-code inside of history directory
-qrcode.save(f"{os.path.dirname(__file__)}/history/qrCode.png", scale=20, dark="yellow", light="#323524")
+qrcode.save(f"{os.path.dirname(__file__)}/history/qrCode.png", scale=20, dark="yellow", light="#35155D")
 
 # Add /home/zed/.local/bin to the PATH variable of the current process
 os.environ["PATH"] += os.pathsep + "/home/zed/.local/bin"
